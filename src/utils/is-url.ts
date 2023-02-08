69f160c357a38eb277d8732b8e80222721498302
Aviv Haruzi <avivharuzi@gmail.com>
2023-02-08 20:07:55 +0200

    add utils functions

--- src/utils/is-url.ts
+++ src/utils/is-url.ts
@@ -1,13 +1,13 @@
 export const isURL = (str: string): boolean => {
   const pattern = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '^(https?:\\/\\/)?' + // HTTP protocol.
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // Domain name.
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // Port and path.
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // Query string.
+      '(\\#[-a-z\\d_]*)?$', // Fragment locater.
     'i'
-  ); // fragment locater
+  );
 
   return pattern.test(str);
 };
