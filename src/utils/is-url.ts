7c06d6aefcdff45d6f59f64630d2bd26a17fada1
Aviv Haruzi <avivharuzi@gmail.com>
2023-02-08 22:48:30 +0200

    add utils functions

--- src/utils/is-url.ts
+++ src/utils/is-url.ts
@@ -1,13 +1,5 @@
 export const isURL = (str: string): boolean => {
-  const pattern = new RegExp(
-    '^(https?:\\/\\/)?' + // HTTP protocol.
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // Domain name.
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // Port and path.
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // Query string.
-      '(\\#[-a-z\\d_]*)?$', // Fragment locater.
-    'i'
-  );
+  const pattern = /(http(s?)):\/\//i;
 
   return pattern.test(str);
 };
