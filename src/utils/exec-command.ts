865f97208dddefd3fa2550121394be11ac426207
Aviv Haruzi <avivharuzi@gmail.com>
2023-02-10 13:09:13 +0200

    update exec

--- src/utils/exec-command.ts
+++ src/utils/exec-command.ts
@@ -5,7 +5,7 @@ import { exec } from 'node:child_process';
  */
 export const execCommand = (command: string): Promise<string> => {
   return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
+    exec(command, (error, stdout) => {
       if (error) {
         reject(error);
 
